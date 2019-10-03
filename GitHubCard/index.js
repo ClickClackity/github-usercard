3751cdb3acaeb89d8581f6d94ca961ee417264b1
Clackity <lockout18@yahoo.com>
2019-10-03 17:39:33 -0500

    mvp+stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,88 @@
+const ac = (parent, child) => parent.appendChild(child);
+const ele = use => document.createElement(use);
+
+const cghc = user => {
+  const card = ele('div'),
+    image = ele('img'),
+    info = ele('div'),
+    name = ele('h3'),
+    username = ele('p'),
+    location = ele('p'),
+    profile = ele('p'),
+    profile_link = ele('a');
+    followers = ele('p'),
+    following = ele('p'),
+    bio = ele('p');
+    //
+    image.src = user.avatar_url;
+    name.textContent = user.name;
+    username.textContent = user.username;
+    location.textContent = !(user.location === null) ? `Location: ${user.location}` : `Location: Not Currently Known`;
+    profile.textContent = `Profile: `;
+    profile_link.textContent = user.html_url;
+    profile_link.setAttribute('href', user.html_url);
+    followers.textContent = `Followers: ${user.followers}`;
+    following.textContent = `Following: ${user.following}`;
+    bio.textContent = `Bio: ${user.bio}`; 
+    //
+    username.classList.add('username');
+    name.classList.add('name');
+    card.classList.add('card');
+    info.classList.add('card-info');
+    //
+    ac(card, image);
+    ac(card, info);
+    ac(info, name);
+    ac(info, username);
+    ac(info, location);
+    ac(info, profile);
+    ac(profile, profile_link);
+    ac(info, followers);
+    ac(info, following);
+    ac(info, bio);
+    ac(document.querySelector('.cards'), card);
+    //  
+};
+
+const findUser = (s, n) => axios.get(`https://api.github.com/users/${n}`).then(response => {
+  cghc(response.data);
+  if (n == s) {
+    findFollowers(s, response.data.followers_url);
+  }
+}).catch(error => console.log(`Error: ${error}`));
+
+const findFollowers = (s, f) => {
+  axios.get(f).then(response => {
+    response.data.forEach(f => findUser(s, f.login));
+  }).catch(error => console.log(`Error: ${error}`));
+};
+
+findUser('clickclackity', 'clickclackity')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
@@ -24,10 +109,8 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+          Using DOM methods and properties, create a component that will return the following DOM ele:
 
 <div class="card">
   <img src={image url of user} />
